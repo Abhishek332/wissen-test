ab509a08102313e079ac4617ce287bc548ca4df9
Abhishek332 <itsabhiofficial.21@gmail.com>
2022-08-04 11:18:49 +0530

    Routes addd

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import { Authenticator } from "./pages";
+import { Authenticator, Home } from "./pages";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Authenticator />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Authenticator />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
