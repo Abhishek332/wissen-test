4caab1949276108f6ffce0a10217da165f25ceaa
Abhishek332 <itsabhiofficial.21@gmail.com>
2022-08-04 11:21:23 +0530

    Some changes

--- src/pages/Authenticator/Authenticator.js
+++ src/pages/Authenticator/Authenticator.js
@@ -2,21 +2,31 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { userLoginCall } from "../../redux/action";
+import Logo from "./logo.png";
 // import { storeType } from '../redux/types/storeType';
 
-import "./Authenticator.css";
+import "./Authenticator.scss";
 import { ToastBox, useToaster } from "../../utils/toaster";
 import { LOGIN_NULL } from "../../redux/types/actionTypes";
+import { useNavigate } from "react-router-dom";
+import { InputBox } from "../../components";
 
 const Authenticator = () => {
-  const [formValue, setFormValue] = useState({ email: "", password: "" }),
+  const [formValue, setFormValue] = useState({
+      email: "",
+      password: "",
+      checkbox: false,
+    }),
     dispatch = useDispatch(),
     toaster = useToaster(),
-    { loading, userInfo, error } = useSelector((state) => state.userLogin);
+    navigate = useNavigate(),
+    { loading, userInfo, error } = useSelector((state) => state.userLogin),
+    alreadyLogged = JSON.parse(localStorage.getItem("userInfo") || "null");
+
+  if (alreadyLogged) navigate("/home");
 
   useEffect(() => {
     if (!error) return;
-    console.log(error);
     toaster("error", error);
     dispatch({ type: LOGIN_NULL });
   }, [error, dispatch, toaster]);
@@ -31,31 +41,60 @@ const Authenticator = () => {
       const { name, value } = e.target;
       setFormValue({ ...formValue, [name]: value });
     },
-    handleSubmit = () => dispatch(userLoginCall(formValue));
+    handleSubmit = (e) => {
+      e.preventDefault();
+      dispatch(userLoginCall(formValue));
+    };
 
-  if (loading) return <h1>Loading...</h1>;
+  if (loading) return <h1 className="loading">Loading...</h1>;
+
+  const InputFields = [
+    {
+      name: "email",
+      type: "email",
+      value: formValue.email,
+      handleChange,
+      label: "Email",
+    },
+    {
+      name: "password",
+      type: "password",
+      value: formValue.password,
+      handleChange,
+      label: "Password",
+    },
+  ];
 
   return (
     <>
-      <div className="form">
-        <input
-          type="email"
-          name="email"
-          placeholder="Enter your email*"
-          value={formValue.email}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password*"
-          value={formValue.password}
-          onChange={handleChange}
-          required
-        />
-        <button onClick={handleSubmit}>Submit</button>
-      </div>
+      <form
+        onSubmit={handleSubmit}
+        className="form-box flex flex-col justify-center"
+      >
+        <div className="logo">
+          <img src={Logo} alt="" />
+        </div>
+        <p>Hello there, SignIn to continue</p>
+        {InputFields.map((inputData, index) => (
+          <InputBox key={`input-${index + 1}`} {...inputData} />
+        ))}
+        <div className="flex check">
+          <InputBox
+            type="checkbox"
+            name="checkbox"
+            value={formValue.checkbox}
+            handleChange={handleChange}
+            required
+          />
+          <p>
+            By creating or logging into an account, you are agreeing with our{" "}
+            <span>Terms & Conditions</span> and <span>Privacy Policys.</span>
+          </p>
+        </div>
+        <button>Next</button>
+        <h4>Signin with company SSO</h4>
+      </form>
+
       <ToastBox />
     </>
   );
